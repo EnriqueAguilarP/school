a87e758c2c1b8311000c0987b353622a680c1ba8
Jesus Aguilar <enrique.prado207@gmail.com>
2023-05-11 11:02:20 -0600

    Add Redux functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Error404 from "containers/errors/Error404";
 import Home from "containers/pages/Home";
+import store from "store";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={ <Error404 />} />
-        <Route path="/" element= { <Home /> } />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="*" element={ <Error404 />} />
+            <Route path="/" element= { <Home /> } />
+          </Routes>
+        </Router>
+    </Provider>
+  
   );
 }
 
