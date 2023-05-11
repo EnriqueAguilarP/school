250098bd455c1192f60bff4787314f8762293dea
Jesus Aguilar <enrique.prado207@gmail.com>
2023-05-10 21:04:07 -0600

    Add React Router, Routes and Home Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <h1>Hola</h1>
+    <Router>
+      <Routes>
+        <Route path="*" element={ <Error404 />} />
+        <Route path="/" element= { <Home /> } />
+      </Routes>
+    </Router>
   );
 }
 
